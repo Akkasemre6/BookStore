e520619c0895798ea584e2eae457e8ca5d40b5c0
Emre <akkasemre0021@gmail.com>
2025-04-02 18:21:19 +0300

    backend completed

--- src/main/java/com/example/bookstore/BookstoreApplication.java
+++ src/main/java/com/example/bookstore/BookstoreApplication.java
@@ -3,8 +3,12 @@ package com.example.bookstore;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan("com.example.bookstore.Entity")
+@EnableJpaRepositories("com.example.bookstore.Repository")
 public class BookstoreApplication implements CommandLineRunner {
 
 
